18b016a5ff0663861f0acd9ffd26eff8e5c5aaf4
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-07-24 09:52:34 -0400

    added more features

--- src/app/component/LinkBar.jsx
+++ src/app/component/LinkBar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function LinkBar({link, top, left, position='absolute', src='assets/images/icon-arrow-right.svg'}) {
+export default function LinkBar({link, top, left, position='absolute', src='assets/images/icon-arrow-right.svg', width, padding}) {
   return (
-    <div className={`${link == 'github' ?  'bg-[#1E1E1E]' : link == 'youtube' ? 'bg-[#EE3939]' : 'bg-[#2D68FF]'} flex justify-between text-white left-[24%] xl:left-[11%] xl:h-[45px] xl:w-[240px] rounded text-xs w-[158px] h-[30px] items-center px-3`} style={{top, position, left}}>
+    <div className={`${link == 'github' ?  'bg-[#1E1E1E]' : link == 'youtube' ? 'bg-[#EE3939]' : 'bg-[#2D68FF]'} flex justify-between text-white left-[24%] xl:left-[11%] xl:h-[45px] xl:w-[240px] rounded text-xs w-[158px] h-[30px] items-center px-3`} style={{top, position, left, width, padding}}>
        <span>{link = link[0].toUpperCase() + link.slice(1)}</span>
        <span><img className='w-2/3' src={src} /></span>
     </div>
--- src/app/preview/[...username]/page.js
+++ src/app/preview/[...username]/page.js
@@ -3,13 +3,14 @@ import LinkBar from "@/app/component/LinkBar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Suspense } from "react";
 
 export default function page() {
   const pathname = usePathname();
   const id = useState(pathname.split("/")[2]);
   const [data, setData] = useState({});
   const [links, setLinks] = useState([]);
-  // console.log(pathname.split('/')[2])
+  let [showInfo, setShowInfo] = useState(false)
   useEffect(() => {
     async function getUser() {
       const data = await fetch("/api/preview", {
@@ -18,62 +19,75 @@ export default function page() {
       });
       if (data.ok) {
         const response = await data.json();
-        console.log(response);
         if (response.message == "user not found") {
           setData({});
           setLinks([]);
-          return;
+          setShowInfo(true)
+          return
         }
-        console.log(response);
         setData({ ...response });
         setLinks(response.links);
+        setShowInfo(false)
       }
-      // console.log(response.links)
     }
     getUser();
   }, []);
+  if (showInfo) {
+    return (
+      <>
+        <div className="w-screen h-screen text-center py-10">
+          <h2 className="text-3xl">404</h2>
+          <div className="mt-5 font-bold">User not found</div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="h-screen">
       {data.loggedIn ? (
-        <div className="flex justify-between p-5">
-          <Link
-            href="/editor"
-            className="py-2 px-4 border h-full flex items-center rounded border-solid border-[#633CFF] text-[#633CFF]"
-          >
-            Back to Editor
-          </Link>
-          <Link href="" className=" text-white bg-purple-600 rounded py-2 px-6">
-            Share Link
-          </Link>
-        </div>
-      ) : Object.keys(data).length == 0 ? (
-        <div className="text-center">
-          <span className="block py-5 w-full text-[5em]">404</span>
-          <div className="flex w-full items-center justify-center">
-            User not found
+        <Suspense fallback={<p>loading</p>}>
+          <div className="flex justify-between p-5">
+            <Link
+              href="/editor"
+              className="py-2 px-4 border h-full flex items-center rounded border-solid border-[#633CFF] text-[#633CFF]"
+            >
+              Back to Editor
+            </Link>
+            <Link
+              href=""
+              className=" text-white bg-purple-600 rounded py-2 px-6"
+            >
+              Share Link
+            </Link>
           </div>
-        </div>
+        </Suspense>
       ) : (
         <></>
       )}
-      <div className="flex flex-col h-full items-center justify-center">
+
+      <div className="flex flex-col p-7 items-center justify-center">
         <span>
-          <h2>{data ? data.full_name : <>User not found</>}</h2>
+          <h2 className="text-3xl font-semibold pb-4">
+            {data ? data.full_name : <>User not found</>}
+          </h2>
         </span>
         <span> {data ? data.email : <></>}</span>
-        <div>
+        <div className="w-full flex flex-col items-center justify-center">
           {links.length > 0 ? (
             links
               .map((el) => el[Object.keys(el)])
               .map((el) =>
                 el?.id != undefined ? (
-                  <div className="my-5">
-                    <a href={el.link} target="_blanck">
+                  <div className="my-2">
+                    <a href={el.link} className="" target="_blanck">
                       <LinkBar
                         position="relative"
                         src="\assets\images\icon-arrow-right.svg"
                         link={el.platform}
                         left="0"
+                        width="230px"
+                        padding="20px"
                       />
                     </a>
                   </div>
@@ -84,8 +98,6 @@ export default function page() {
           ) : (
             <></>
           )}
-
-          {/* {data.links.map(el => <ShareLink />)} */}
         </div>
       </div>
     </div>
